e77f205e0a26440ca94d433766857e6527a7613b
Tebogo <tebogomakhubu1@gmail.com>
2025-07-01 16:26:31 +0200

    git rm --cached the extra silver dbt models

--- zillow_pipeline/models/silver/silver_zillow_regions_counties.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    region_id,
-    SPLIT_PART(region_info, ';', 1) AS county,
-    SPLIT_PART(region_info, ';', 2) AS state_abbr
-FROM {{ref ('bronze_zillow_regions') }}
-WHERE region_type = 'county'
